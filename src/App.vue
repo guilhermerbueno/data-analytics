9a7e2a91044eb381918907880691c16013448b34
Guilherme Rodrigues Bueno <gbueno91@gmail.com>
2021-10-04 19:00:16 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="app">
+  <div
+    data-analytics-show 
+    data-analytics-event="my-page-click"
+    data-analytics-event-category="my-application:my-page"
+    data-analytics-event-action="click:my-button"
+    data-analytics-event-label="my-label-button"
+    data-analytics-screen-name="my-page"
+    data-analytics-dimension-22="my-dimension-value"
+    id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AutomaticAnalyticsTrigger from 'automatic-analytics-trigger';
 
 Vue.config.productionTip = false
+
+const dataLayer = [];
+const automaticAnalyticsTrigger = new AutomaticAnalyticsTrigger((dataEvent) => dataLayer.push(dataEvent));
+
+automaticAnalyticsTrigger.init();
 
 new Vue({
   render: h => h(App),
